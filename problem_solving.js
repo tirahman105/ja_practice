401ec76926ff77538d73637d0a9c9d36d5101283
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-01-29 08:06:30 +0600

    practice

--- problem_solving.js
+++ problem_solving.js
@@ -1,4 +1,4 @@
-var moneyGiven = 1000;
+/* var moneyGiven = 1000;
 var applePrice = 400;
 var orangePrice = 300;
 
@@ -6,3 +6,14 @@ var expense = applePrice + orangePrice ;
 var remainingAmount = moneyGiven - expense;
 
 console.log(remainingAmount);
+ */
+
+var totalAmount = parseFloat(prompt("Enter your total amount"));
+var applePrice =  parseFloat(prompt("Enter apple Price"));
+var orangePrice =  parseFloat(prompt("Enter orange Price"));
+
+var expense = applePrice + orangePrice ;
+var remainingAmount = totalAmount - expense;
+
+document.write("Your expense is " + expense + "<br>");
+document.write("Your remaining amount is " + remainingAmount );
