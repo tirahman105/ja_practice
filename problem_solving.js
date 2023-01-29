b35ef3ab4d3d3e6126edcfb6b2ff74da13954a7b
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-01-29 07:44:25 +0600

    practice

---
+++ problem_solving.js
@@ -0,0 +1,8 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var expense = applePrice + orangePrice ;
+var remainingAmount = moneyGiven - expense;
+
+console.log(remainingAmount);
