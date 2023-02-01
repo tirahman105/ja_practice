d075e3e65046dd997dfcb9d99550545e96c9c5bd
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-01 21:36:46 +0600

    function loop

---
+++ function_basic.js
@@ -0,0 +1,19 @@
+function add(num1, num2){
+    var result = num1 + num2;
+    return result;
+}
+function minus(num1, num2){
+    var result = num1 - num2;
+    return result;
+}
+function multiply(num1, num2 , num3){
+    var result = num1 * num2 * num3;
+    return result;
+}
+var totalSum= add(13,12);
+var totalMinus= minus(13,12);
+var totalMultiply= multiply(10,2,3);
+
+console.log(totalSum);
+console.log(totalMinus);
+console.log(totalMultiply);
---
+++ loop_text.js
@@ -0,0 +1,17 @@
+
+/* function say_sorry(name){
+    var text = "sorry";
+for( var i = 0; i < 10; i++ ){
+    console.log(text + " "+ name);
+    
+}
+}
+say_sorry("text"); */
+
+function love (wife){
+    for( var i = 0 ; i < 10 ; i ++){
+        console.log("I LOVE YOU! " + wife);
+    }
+
+}
+love("Anonna");
