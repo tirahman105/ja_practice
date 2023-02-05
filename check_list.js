dc3558a93e8dc56d1d68f5e09b8d84d8514de538
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-06 00:16:33 +0600

    check list 1

---
+++ check_list.js
@@ -0,0 +1,80 @@
+// ১. তোমাকে যদি বলি একটা স্ট্রিং টাইপের ভেরিয়েবল লিখো। একটা বুলিয়ান টাইপের ভেরিয়েবল লিখো বা একটা নাম্বার টাইপের ভেরিয়েবল লিখো। সেটা কি তুমি লিখতে পারবে? যদি পারো তাহলে ভিজুয়াল ষ্টুডিও কোড খুলে লিখে ফেলো। 
+
+// string variable
+var name = "Tareq";
+
+// Bulian variable
+var married = true;
+
+// number variable
+var age = 31;
+
+
+// --------------------------------------------
+
+// ২. তুমি কি জানো কখন let দিয়ে ভেরিয়েবল লিখতে হয়ে আর কখন const দিয়ে ভেরিয়েবল লিখতে হয়। যদি লিখতে পারো তাহলে লিখে ফেলো। এবং যেই যেটা দিয়ে ভেরিয়েবল লিখলে আবার সেটা চেইঞ্জ করা যায়। তাহলে যেটা দিয়ে ভেরিয়েবল ডিক্লেয়ার করলে চেইঞ্জ করা যায়। সেটার মান চেইঞ্জ করে নাও । অর্থাৎ জাস্ট দুইটা ভেরিয়েবল ডিক্লেয়ার করবে let এবং const দিয়ে। তারপর যেকোন একটার ভ্যালু চেইঞ্জ করবে। এই let এবং const মাথা গরম করে দিলে টেনশন নিয়ে না। সামনের মাইলস্টোন এর পরের মাইলস্টোন এ এইটা নিয়ে আরো ডিসকাস করা হবে। 
+
+
+// let and const 
+
+// we can change the vairable if we declare with let.
+
+let computer = "Lenovo";
+
+console.log(computer);
+
+computer = "Samsung";
+console.log(computer);
+
+
+
+// -----------------------------
+// ৩. দুইটা ভেরিয়েবল এর মধ্যে যোগ, বিয়োগ, গুণ, ভাগ কিভাবে করতে হয় সেটা কি জানো। অর্থাৎ তুমি কি +, -, *, /, %এইগুলার ব্যবহার জানো। তাহলে নাম্বার টাইপের দুইটা ভেরিয়েবল লিখো তারপর তাদের যোগ করে সেটার মান আরেকটা ভেরিয়েবল এ রাখো। একইভাবে ওই দুইটা ভেরিয়েবল এর মধ্যে বিয়োগ, গুন, ভাগ এবং ভাগশেষ বের করো। 
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+console.log(sum);
+
+// Math.abs is uses to see the difference between two numbers
+
+let diff = Math.abs(num1-num2);
+console.log(diff);
+
+let multiply = num1 * num2;
+console.log( "Multiplication : " + multiply);
+
+let divide = num1 / num2;
+console.log("devide : " , divide);
+
+let remainder = num1 % num2;
+console.log(remainder);
+
+// -----------------------------
+// ৪. তুমি কি দুইটা ভেরিয়েবল এরমধ্যে তুলনা করতে পারো। কম্পারিজন করতে পারো। যে দুইটা ভেরিয়েবল এর মধ্যে প্রথমটা সেকেন্ডটা এর চাইতে ছোট, বড়, অসমান, সমান , ছোট বা সমান, বড় বা সমান। এইগুলা চেক করতে পারো। অর্থাৎ <, >, ==, !=, <=, >= চিহ্নগুলা ব্যবহার করতে পারো। তাহলে দুইটা সংখ্যা টাইপের ভেরিয়েবল ডিক্লেয়ার করে তাদেরকে এই ছয়ভাবে তুলনা করে কোড লিখে ফেলো।
+
+let comparisonNum1 = 10;
+let comparisonNum2 = 10;
+
+if( comparisonNum1 < comparisonNum2){
+    console.log("Number 2 is larger");
+}else{
+    console.log("Number 1 is larger");
+}
+if( comparisonNum1 == comparisonNum2){
+    console.log("They are equal");
+}else{
+    console.log("They are not equal");
+}
+
+// . তোমাকে যদি বলে একটা while লুপ দিয়ে ৭ থেকে ১৯ পর্যন্ত যতগুলা বিজোড় সংখ্যা আছে সেগুলা দেখাতে। তুমি কি সেটা দেখাতে পারবে? পারলে সেই কোড লিখে ফেলো। 
+
+let number = 7;
+while(number <= 19){
+    if (number % 2 !== 0 ){
+        console.log(number);
+    }
+    number ++;
+}
+
