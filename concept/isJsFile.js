1623bfd27cbf7e8a813a4189347842ab74531b91
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-07 01:58:23 +0600

    final update

--- concept/isJsFile.js
+++ concept/isJsFile.js
@@ -1,10 +1,25 @@
-function isJavascriptFile(fileName){
-    if(fileName.endsWith('.js')){
-        return true;
-    }else{
-        return false;
-    }
+// function isJavascriptFile(fileName){
+//     if(fileName.endsWith('.js')){
+//         return true;
+//     }else{
+//         return false;
+//     }
 
+// }
+// let result = isJavascriptFile("index.js");
+// console.log(result);
+
+function isJavaScriptFile(fileName){
+    if(typeof fileName != 'string' || fileName.length == 0){
+        return "Provide valid input"
+    }
+   if(fileName.endsWith('.js')){
+    return true;
+   }else{
+   return false;
+   }
 }
-let result = isJavascriptFile("index.js");
+
+
+let result = isJavaScriptFile("");
 console.log(result);
