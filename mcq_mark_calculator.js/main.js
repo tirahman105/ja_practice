038de5f79954c77fad0add947f6c618716d526ca
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-07 22:51:08 +0600

    update

---
+++ mcq_mark_calculator.js/main.js
@@ -0,0 +1,31 @@
+// function mcqCalculator(marks){
+//     let correct = marks * 0.2;
+//     return correct;
+// }
+// const result = mcqCalculator(40);
+// console.log(result);
+
+function compare(a, b) {
+
+    if (a.toString() === b) {
+   
+    return true;
+   
+    } else {
+   
+    return false;
+   
+    }
+   
+   }
+   
+   const result = compare(25, 25);
+   
+   console.log(result);
+   console.log("123" + 123);
+
+   let a="hi";
+
+let b="there";
+
+console.log(a+b);
